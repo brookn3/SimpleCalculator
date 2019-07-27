42b4193b47c496f2c316922c3ac7c51c647c372b
brookn3 <brooknegussie2014@gmail.com>
2019-07-26 17:02:36 -0700

    added a clear button for the calculator as well as declaring and initializing all of the objects on the calculator.

--- app/src/main/java/edu/tacoma/uw/css/brookn3/simplecalculator/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/brookn3/simplecalculator/MainActivity.java
@@ -8,19 +8,65 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button numZeroBtn;
+    private Button numOneBtn;
+    private Button numTwoBtn;
+    private Button numThreeBtn;
+    private Button numFourBtn;
+    private Button numFiveBtn;
+    private Button numSixBtn;
+    private Button numSevenBtn;
+    private Button numEightBtn;
+    private Button numNineBtn;
+
+    private Button decimalBtn;
+    private Button addBtn;
+    private Button subtractBtn;
+    private Button multiplyBtn;
+    private Button divideBtn;
+    private Button clearBtn;
+
+    private EditText calcEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button plusSignBtn = (Button) findViewById(R.id.plusSignBtn);
+        // Initializing fields:
+        setupUIViews();
 
-        // Assigning an onClick listener:
-        plusSignBtn.setOnClickListener(new View.OnClickListener() {
+
+
+        // Defining a listener for each object:
+        numZeroBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText calcEditText = (EditText) findViewById(R.id.calcEditText);
+                calcEditText.setText(calcEditText.getText().toString() +
+                                        numZeroBtn.getText().toString());
             }
         });
+    }
+
+    private void setupUIViews() {
+        numZeroBtn  = (Button) findViewById(R.id.numZeroBtn);
+        numOneBtn = (Button) findViewById(R.id.numOneBtn);
+        numTwoBtn = (Button) findViewById(R.id.numTwoBtn);
+        numThreeBtn = (Button) findViewById(R.id.numThreeBtn);
+        numFourBtn = (Button) findViewById(R.id.numFourBtn);
+        numFiveBtn = (Button) findViewById(R.id.numFiveBtn);
+        numSixBtn = (Button) findViewById(R.id.numSixBtn);
+        numSevenBtn = (Button) findViewById(R.id.numSevenBtn);
+        numEightBtn  = (Button) findViewById(R.id.numEightBtn);
+        numNineBtn = (Button) findViewById(R.id.numNineBtn);
+
+        decimalBtn = (Button) findViewById(R.id.decimalBtn);
+        addBtn = (Button) findViewById(R.id.addBtn);
+        subtractBtn = (Button) findViewById(R.id.subtractBtn);
+        multiplyBtn = (Button) findViewById(R.id.multiplyBtn);
+        divideBtn = (Button) findViewById(R.id.divideBtn);
+        clearBtn = (Button) findViewById(R.id.clearBtn);
+
+        calcEditText = (EditText) findViewById(R.id.calcEditText);
     }
 }
