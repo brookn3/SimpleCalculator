836894600d29019c2f43afa3e6be69fda6421cd8
brookn3 <brooknegussie2014@gmail.com>
2019-07-29 18:07:04 -0700

    added clear functionality.

--- app/src/main/java/edu/tacoma/uw/css/brookn3/simplecalculator/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/brookn3/simplecalculator/MainActivity.java
@@ -176,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
         clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                calcEditText.setText(new String());
             }
         });
 
