819be5edf860e7f9742784f8f8a53500f0e41466
brookn3 <brooknegussie2014@gmail.com>
2019-08-10 11:51:45 -0700

    implemented the addition and subtraction calculations to work properly when the equal button is pressed. Also refactored the code by adding a helper method onClickHelper which includes a Toast  message.

--- app/src/main/java/edu/tacoma/uw/css/brookn3/simplecalculator/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/brookn3/simplecalculator/MainActivity.java
@@ -2,10 +2,13 @@ package edu.tacoma.uw.css.brookn3.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,10 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText calcEditText;
 
-    private StringBuilder currentValue;
-    private List<Double> inputValues;
-    private List<String> arithmeticOperators;
+    private List<Object> expressionList;
     private double result;
+
+    /* Once a user presses the equals sign,
+     * all buttons except the Clear will be disabled.
+     */
+    private boolean isResultDisplayed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,98 +51,88 @@ public class MainActivity extends AppCompatActivity {
 
         // Initializing fields:
         setupUIViews();
-        currentValue = new StringBuilder();
-        inputValues = new LinkedList<Double>();
-        arithmeticOperators = new LinkedList<String>();
+
+        expressionList = new LinkedList<Object>();
         result = 0;
+        isResultDisplayed = false;
+
 
 
         // Defining a listener for each object:
         numZeroBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numZeroBtn.getText().toString());
+                onClickHelper(numZeroBtn.getText().toString());
             }
         });
 
         numOneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numOneBtn.getText().toString());
+                onClickHelper(numOneBtn.getText().toString());
             }
         });
 
         numTwoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numTwoBtn.getText().toString());
+                onClickHelper(numTwoBtn.getText().toString());
             }
         });
 
         numThreeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numThreeBtn.getText().toString());
+                onClickHelper(numThreeBtn.getText().toString());
             }
         });
 
         numFourBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numFourBtn.getText().toString());
+                onClickHelper(numFourBtn.getText().toString());
             }
         });
 
         numFiveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numFiveBtn.getText().toString());
+                onClickHelper(numFiveBtn.getText().toString());
             }
         });
 
         numSixBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numSixBtn.getText().toString());
+                onClickHelper(numSixBtn.getText().toString());
             }
         });
 
         numSevenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numSevenBtn.getText().toString());
+                onClickHelper(numSevenBtn.getText().toString());
             }
         });
 
         numEightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numEightBtn.getText().toString());
+                onClickHelper(numEightBtn.getText().toString());
             }
         });
 
         numNineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        numNineBtn.getText().toString());
+                onClickHelper(numNineBtn.getText().toString());
             }
         });
 
         decimalBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        decimalBtn.getText().toString());
+                onClickHelper(decimalBtn.getText().toString());
             }
         });
 
@@ -144,32 +140,28 @@ public class MainActivity extends AppCompatActivity {
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        addBtn.getText().toString());
+                onClickHelper(addBtn.getText().toString());
             }
         });
 
         subtractBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        subtractBtn.getText().toString());
+                onClickHelper(subtractBtn.getText().toString());
             }
         });
 
         multiplyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        multiplyBtn.getText().toString());
+                onClickHelper(multiplyBtn.getText().toString());
             }
         });
 
         divideBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcEditText.setText(calcEditText.getText().toString() +
-                        divideBtn.getText().toString());
+                onClickHelper(divideBtn.getText().toString());
             }
         });
 
@@ -177,25 +169,194 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 calcEditText.setText(new String());
+
+                isResultDisplayed = false;
             }
         });
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String current = calcEditText.getText().toString();
-                if (!current.isEmpty()) {
-                    calcEditText.setText(current.substring(0, current.length() - 1));
+                if (isResultDisplayed) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Please press the Clear button first.",
+                            Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                } else {
+                    String current = calcEditText.getText().toString();
+                    if (!current.isEmpty()) {
+                        calcEditText.setText(current.substring(0, current.length() - 1));
+                    }
                 }
             }
         });
+
+
+
+
+
+
+
+
+
+
+
+
 
         equalsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                final String strExpression = calcEditText.getText().toString();
+
+                if (!strExpression.isEmpty() && !isResultDisplayed) {
+
+                    // Disabling every other button except the Clear button.
+                    isResultDisplayed = true;
+
+                    expressionList = new LinkedList<Object>();
+                    result = 0;
+
+                    boolean isDouble = false;
+                    int exprSize = strExpression.length();
+                    String[] operators = {"+", "-", "*", "/"};
+
+                    // Capturing the first token:
+                    String previous = strExpression.substring(0, 1);
+                    String current;
+
+                    // Traversing the strExpression to capture the operators and operands
+                    // and building on the "expressionList" LinkedList:
+                    for (int i = 1; i < exprSize; i++) {
+                        current = strExpression.substring(i, i + 1);
+
+                        if (current.equals(".")) { // The number is a decimal
+                            isDouble = true;
+
+                            previous = previous + current;
+                        } else if (current.equals(operators[0]) || current.equals(operators[1]) ||
+                                current.equals(operators[2]) || current.equals(operators[3])) {
+
+                            expressionList.add(previous); // Still stored as a String Object.
+                            expressionList.add(current);
+
+                            previous = new String();
+
+                        } else { // TODO: Make sure to check for all possible inputs later!!!
+                            previous = previous + current;
+
+                            if (i == exprSize - 1) { // last digit
+                                expressionList.add(previous);
+                            }
+                        }
+                    }
+
+
+                    // Traversing the newly built list (LinkedList) and
+                    // completing the calculation following the PEMDOS property:
+                    Iterator leader = expressionList.iterator();
+
+
+
+
+//                    Iterator follower = expressionList.iterator();
+//                    Iterator ff = expressionList.iterator(); // The follower's follower.
+//
+//                    // Used to make sure the three iterators are in
+//                    // one line instead of jumping ahead of one another.
+//                    int counter = 0;
+//
+//                    // Computing any multiplication and division here:
+//                    while (leader.hasNext()) {
+//                        String currentNode = (String) leader.next();
+//
+//                        if (currentNode.equals(operators[2])) {
+//
+//
+//                        } else if (currentNode.equals(operators[3])) {
+//
+//                        } else {
+//                            if (counter >= 1) {
+//                                follower.next();
+//                            }
+//                            if (counter >= 2) {
+//                                ff.next();
+//                            }
+//                        }
+//
+//                        counter++;
+//                    }
+
+
+
+                    // Resetting iterators:
+                    leader = expressionList.iterator();
+
+                    double secondOperand = 0;
+                    boolean isAddition = false;
+                    boolean isSubtraction = false;
+
+                    int indexCounter = 0;
+                    String currentNode;
+
+                    // Computing addition and subtraction here:
+                    while (leader.hasNext()) {
+                        currentNode = (String) leader.next();
+
+                        if (currentNode.equals(operators[0])) { // Addition:
+                            isAddition = true;
+                        } else if (currentNode.equals(operators[1])) { // Subtraction:
+                            isSubtraction = true;
+                        } else {
+
+                            // Capturing the values:
+                            if (indexCounter == 0) {
+                                result = Double.parseDouble(currentNode); // TODO: change code b/c this will destroy the value developed from the code above
+                            } else if (indexCounter % 2 == 0) {
+                                secondOperand = Double.parseDouble(currentNode);
+
+                                // Calculating part of the expression:
+                                if (isAddition) {
+                                    result += secondOperand;
+                                } else if (isSubtraction) {
+                                    result -= secondOperand;
+                                }
+
+
+                                // Resetting the operands check:
+                                isAddition = false;
+                                isSubtraction = false;
+                            }
+                        }
+
+                        indexCounter++;
+                    }
+
+                    // Result:
+                    if (isDouble) { // TODO: Figure out how you want to display the result & make
+                                    // TODO: sure to put an equals sign prior to displaying the result:
+                        calcEditText.setText(strExpression + "=" + result);
+                    } else {
+                        calcEditText.setText(strExpression + "=" + ((int) result));
+                    }
+                }
             }
         });
+    }
+
+
+    private void onClickHelper(final String display) {
+        if (isResultDisplayed) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Please press the Clear button first.",
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        } else {
+            calcEditText.setText(calcEditText.getText().toString() +
+                    display);
+        }
     }
 
     private void setupUIViews() {
